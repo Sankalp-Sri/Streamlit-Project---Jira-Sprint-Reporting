cf860c9a07a0b8d4707cb91a06e2af4906550d98
Sankalp Srivastava <57254548+Sankalp-Sri@users.noreply.github.com>
2025-02-27 16:54:53 +0530

    Create config_prod.py

---
+++ config/config_prod.py
@@ -0,0 +1,15 @@
+
+from datetime import datetime,date
+
+
+
+#Current_time
+current_date = date.today()
+current_datetime = datetime.now()
+
+app_link='https://platsprintreporting.livecareer.com'
+
+#google sheet details
+
+sprint_data_sheet_name = 'Sprint Reporting Prod'
+sprint_data_worksheet_name = 'Report Data'
