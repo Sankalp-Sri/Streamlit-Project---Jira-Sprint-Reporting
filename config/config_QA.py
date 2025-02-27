e28a3952894cecc4bd295935a4492273f5613d8e
Sankalp Srivastava <57254548+Sankalp-Sri@users.noreply.github.com>
2025-02-27 16:54:30 +0530

    Create config_QA.py

---
+++ config/config_QA.py
@@ -0,0 +1,16 @@
+
+from datetime import datetime,date
+
+
+
+
+#Current_time
+current_date = date.today()
+current_datetime = datetime.now()
+
+app_link='https://qa-platsprintreporting.livecareer.com'
+
+#google sheet details
+
+sprint_data_sheet_name = 'Sprint Reporting QA'
+sprint_data_worksheet_name = 'Report Data'
